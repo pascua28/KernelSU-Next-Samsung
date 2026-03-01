3b83eb93eb04336ef74de33a09010673c47634ce
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2026-03-01 21:15:56 +0800

    kernel: Provide own stackprotector symbol if necessary
    LineageOS uses a too new clang version to compile GKI kernel, and as a
    result, -mstack-protector-guard=sysreg is recognized by clang. In the end,
    CC_HAVE_STACKPROTECTOR_SYSREG is enabled and make __stack_chk_guard
    symbol not exported. Thus, provide our own symbol when necessary.
    
    ABI compatibility can be guaranteed because no task struct symbol will
    be changed.

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -17,6 +17,22 @@
 #include "ksu_defex.h"
 #include "manager.h"
 
+// workaround for A12-5.10 kernel
+// Some third-party kernel (e.g. linegaeOS) uses wrong toolchain, which supports
+// CC_HAVE_STACKPROTECTOR_SYSREG while gki's toolchain doesn't.
+// Therefore, ksu lkm, which uses gki toolchain, requires this __stack_chk_guard,
+// while those third-party kernel can't provide.
+// Thus, we manually provide it instead of using kernel's
+#if defined(CONFIG_STACKPROTECTOR) && !defined(CONFIG_STACKPROTECTOR_PER_TASK)
+#include <linux/stackprotector.h>
+#include <linux/random.h>
+unsigned long __stack_chk_guard __ro_after_init
+    __attribute__((visibility("hidden")));
+#define NO_STACK_PROTECTOR_WORKAROUND __attribute__((no_stack_protector))
+#else
+#define NO_STACK_PROTECTOR_WORKAROUND
+#endif
+
 struct cred *ksu_cred;
 
 static int manager_uid = -1;
@@ -25,6 +41,16 @@ MODULE_PARM_DESC(manager_uid, "KernelSU Manager UID");
 
 int __init kernelsu_init(void)
 {
+#if defined(CONFIG_STACKPROTECTOR) && !defined(CONFIG_STACKPROTECTOR_PER_TASK)
+    unsigned long canary;
+
+    /* Try to get a semi random initial value. */
+    get_random_bytes(&canary, sizeof(canary));
+    canary ^= LINUX_VERSION_CODE;
+    canary &= CANARY_MASK;
+    __stack_chk_guard = canary;
+#endif
+
 	int ret;
 
 	pr_info("kernelsu: initializing KernelSU-Next LKM\n");
