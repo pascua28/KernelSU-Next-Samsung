73920bd0dfe09d51a182e7950b0355f7d875978d
Ylarod <me@ylarod.cn>
2026-03-12 12:54:12 +0800

    [PARTIAL] feat: jailbreak by Magica (https://github.com/tiann/KernelSU/pull/3268)
    Co-authored-by: 5ec1cff <ewtqyqyewtqyqy@gmail.com>
    Co-authored-by: YuKongA <70465933+YuKongA@users.noreply.github.com>
    Co-authored-by: KOWX712 <leecc0503@gmail.com>
    
    -Note: This change adds an update to sepolicy.
           Magica has been stripped.

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -6,6 +6,7 @@
 #include <linux/workqueue.h>
 
 #include "allowlist.h"
+#include "app_profile.h"
 #include "feature.h"
 #include "klog.h" // IWYU pragma: keep
 #include "manager.h"
@@ -123,12 +124,10 @@ int __init kernelsu_init(void)
 		cache_sid();
 		setup_ksu_cred();
 
-/*
-		if (!getenforce()) {
-			pr_info("Permissive SELinux, enforcing\n");
-			setenforce(true);
-		}
-*/
+		// Grant current process (ksud late-load) root
+		// with KSU SELinux domain before enforcing SELinux, so it
+		// can continue to access /data/app etc. after enforcement.
+		escape_to_root_for_init();
 
 		ksu_allowlist_init();
 		ksu_load_allow_list();
@@ -141,6 +140,11 @@ int __init kernelsu_init(void)
 
 		ksu_boot_completed = true;
 		track_throne(false);
+
+		if (!getenforce()) {
+			pr_info("Permissive SELinux, enforcing\n");
+			setenforce(true);
+		}
 
 	} else {
 		ksu_syscall_hook_manager_init();
