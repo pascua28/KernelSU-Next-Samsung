d1e4cd0c3c1a7f7fafac5e6aeb0f206296b32b2a
pershoot <190600+pershoot@users.noreply.github.com>
2026-03-23 12:15:37 -0400

    manager: Update min. supported kernel version
    -33070:
     -Sepolicy update ('[PARTIAL] feat: jailbreak by Magica (#3268)')

--- manager/app/src/main/java/com/rifsxd/ksunext/Natives.kt
+++ manager/app/src/main/java/com/rifsxd/ksunext/Natives.kt
@@ -18,7 +18,8 @@ object Natives {
     // 11071: Fix the issue of failing to set a custom SELinux type.
     // 12797: zygisk query and get manager uid.
     // 32310: new get_allow_list ioctl
-    const val MINIMAL_SUPPORTED_KERNEL = 32310
+    // 33070: SET_SEPOLICY ioctl
+    const val MINIMAL_SUPPORTED_KERNEL = 33070
 
     const val KERNEL_SU_DOMAIN = "u:r:su:s0"
 
