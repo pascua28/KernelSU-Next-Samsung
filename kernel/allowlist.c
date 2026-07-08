0f93d5c6f2144062ed491251506364548073a8c6
Samuel Pascua <pascua.samuel.14@gmail.com>
2026-07-09 01:00:23 +0800

    kernel: use ksu kallsyms

--- kernel/allowlist.c
+++ kernel/allowlist.c
@@ -393,7 +393,7 @@ static void do_persistent_allow_list(struct callback_head *_cb)
     struct perm_data *p = NULL;
     loff_t off = 0;
 
-    const struct cred *saved = override_creds(ksu_cred);
+    const struct cred *saved = ksu_syms.override_creds(ksu_cred);
     struct file *fp =
         ksu_syms.filp_open(KERNEL_SU_ALLOWLIST, O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (IS_ERR(fp)) {
@@ -424,7 +424,7 @@ static void do_persistent_allow_list(struct callback_head *_cb)
 close_file:
     filp_close(fp, 0);
 out:
-    revert_creds(saved);
+    ksu_syms.revert_creds(saved);
     kfree(_cb);
 }
 
